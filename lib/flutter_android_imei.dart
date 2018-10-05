aae41beec10a63f906b5c095eafb0c764df7d3c8
Eduardo Carísio <eduardo_carisio@hotmail.com>
2018-10-05 17:55:36 -0300

    correction

--- lib/flutter_android_imei.dart
+++ lib/flutter_android_imei.dart
@@ -6,6 +6,7 @@ class FlutterAndroidImei {
   static const MethodChannel _channel = const MethodChannel('flutter_android_imei');
 
   static Future<String> get getImei async {
-    return await _channel.invokeMethod('getImei');
+    String imei = await _channel.invokeMethod('getImei');
+	return imei;
   }
 }
